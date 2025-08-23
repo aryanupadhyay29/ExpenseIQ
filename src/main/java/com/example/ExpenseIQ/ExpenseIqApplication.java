f6802e82f26da3a822ec13e26403de8243342e23
aryanupadhyay29 <aryanupadhyay9027@gmail.com>
2025-08-23 21:37:02 +0530

    Start this Project Here

---
+++ src/main/java/com/example/ExpenseIQ/ExpenseIqApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenseIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseIqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExpenseIQ/ExpenseIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
